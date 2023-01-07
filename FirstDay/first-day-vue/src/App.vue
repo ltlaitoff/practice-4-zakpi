bbc321e2cd73cdd907b8a90cc58dce79ac27c0c8
ltlaitoff <ltlaitoff@gmail.com>
2023-01-08 00:39:09 +0200

    refactor: Formatted App and main

--- FirstDay/first-day-vue/src/App.vue
+++ FirstDay/first-day-vue/src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-import mainImage from '../../img/ph1.jpg'
+
 
 function onClick(e) {
 	const target = e.target
@@ -16,54 +14,11 @@ function onClick(e) {
 		currentHeight >= 500 ? currentHeight - 100 : currentHeight + 50
 	}px`
 }
-
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+	<header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
